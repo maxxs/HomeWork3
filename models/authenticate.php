8e74c15f56bac954166c2f825690d0ba5893d3bb
Sean Traber <smtraber@yahoo.com>
2010-10-31 13:39:13 -0700

    changing authenticate

--- models/authenticate.php
+++ models/authenticate.php
@@ -4,31 +4,19 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 
 function authenticate($username, $password) {
-    $db = mysql_connect("localhost", "root", "");
-    $query ="SELECT UserID FROM Users WHERE UserName=".$username." AND Password=".$password;
-    $result = mysql_query($query);
-    $num_rows = mysql_num_rows($result);
-    $num_fields = mysql_num_fields($result);
-    for($j =1; $j <=$num_rows; $j++)
-    {
-        $row = mysql_fetch_array($result);
-        print $row[0].$row["some_attr"]."<br />";
-    }
-    mysql_close();
-    /*
-    $usernames = array( "sh"=>"shivang", "se"=>"sean");
-    $key = array_search($username, $usernames);
-    $passwords['shivang'] = "shivangshah";
-    $passwords['sean'] = "seantraber";
-    if($key == false)
-    {
-        return false;
-    }
-    if ($passwords[$username] == $password)
+    $configVars = parse_ini_file('../config/config.ini', TRUE);
+    //accessing config variable
+    $uName = $configVars['Database']['username'];
+    $dbPassword = $configVars['Database']['passwd'];
+    $db = mysql_connect("localhost", $uName, $dbPassword);
+    mysql_select_db("URLShortener", $db);
+    $query ="SELECT UserID FROM Users WHERE UserName='".$username."' AND Password='".$password."'";
+    $result = mysql_query($query) or die("Query failed with error: ".mysql_error());
+    
+    while($row = mysql_fetch_array($result)) {
         return true;
-    else
-        return false;
-     *
-     */
+    }
+    mysql_close($db);
+    return false;
 }
 ?>
