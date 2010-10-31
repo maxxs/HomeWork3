da9893c7de6386122aa19b2c65e02396d4d4768b
Shivang Shah <shivangshah15@gmail.com>
2010-10-31 13:53:08 -0700

    added baseurl functionality

--- controllers/signin.php
+++ controllers/signin.php
@@ -1,15 +1,17 @@
 <?php
 session_start();
+$configVars = parse_ini_file('../config/config.ini', TRUE);
+$baseurl = $configVars['Database']['baseurl'];
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 include '../models/authenticate.php';
 $authenticate = authenticate($_POST["username"], $_POST["password"]);
 if ($authenticate) {
     $_SESSION['login'] = '1';
-    header('Location: http://localhost/hw3/index.php?c=default&v=signin_success');
+    header('Location: '.$baseurl.'/index.php?c=default&v=signin_success');
 } else {
     $_SESSION['login'] = '0';
-    header('Location: http://localhost/hw3/index.php?c=default&v=signin_fail');
+    header('Location: '.$baseurl.'/index.php?c=default&v=signin_fail');
 }
 exit;
 ?>
--- views/stats.php
+++ views/stats.php
@@ -1,7 +1,3 @@
 <?php
-    $configVars = parse_ini_file('config/config.ini', TRUE);
-    //accessing config variable
-    echo $configVars['Database']['username'];
-    echo $configVars['Database']['passwd'];
 echo "VIEW STATS";
 ?>
