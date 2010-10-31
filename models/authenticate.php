e6fa7084f1bce9a46fa38345aa020ae4679ee09c
Sean Traber <smtraber@yahoo.com>
2010-10-31 13:15:47 -0700

    changing authenticate

--- models/authenticate.php
+++ models/authenticate.php
@@ -4,6 +4,18 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 
 function authenticate($username, $password) {
+    $db = mysql_connect("localhost", "root", "");
+    $query ="SELECT UserID FROM Users WHERE UserName=".$username." AND Password=".$password;
+    $result = mysql_query($query);
+    $num_rows = mysql_num_rows($result);
+    $num_fields = mysql_num_fields($result);
+    for($j =1; $j <=$num_rows; $j++)
+    {
+        $row = mysql_fetch_array($result);
+        print $row[0].$row["some_attr"]."<br />";
+    }
+    mysql_close();
+    /*
     $usernames = array( "sh"=>"shivang", "se"=>"sean");
     $key = array_search($username, $usernames);
     $passwords['shivang'] = "shivangshah";
@@ -16,5 +28,7 @@ function authenticate($username, $password) {
         return true;
     else
         return false;
+     *
+     */
 }
 ?>
