53f3cadbb7f20ac229a27291d754846fc51111d8
Shivang Shah <shivangshah15@gmail.com>
2010-10-31 13:12:26 -0700

    added config file

--- views/stats.php
+++ views/stats.php
@@ -1,4 +1,7 @@
 <?php
-
+    $configVars = parse_ini_file('config/config.ini', TRUE);
+    //accessing config variable
+    echo $configVars['Database']['username'];
+    echo $configVars['Database']['passwd'];
 echo "VIEW STATS";
 ?>
