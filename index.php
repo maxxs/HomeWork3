ba951881d6b701feb4868919c70048eaa6546a9b
Shivang Shah <shivangshah15@gmail.com>
2010-11-01 20:43:29 -0700

    Final commit with all functionalities working !

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
-include("header.php");
+session_start();
+include ("header.php");
 ?>
 <div class = "body">
     <div class="column2">
             <?php
             $view = $_REQUEST['v'];
-            include_once ("views/$view.php");
+            $controller = $_REQUEST['c'];
+            $key = $_REQUEST['k'];
+            if($controller == "lookup")
+            {
+                include_once ("controllers/lookup.php");
+                lookupAndRedirect($key);
+            }
+            else if($controller == "stats")
+            {
+                include_once ("controllers/stats.php");
+                showStats($_SESSION['UserID']);
+            }
+            else
+            {
+                include_once ("views/$view.php");
+            }
             ?>
     </div>
 </div>
